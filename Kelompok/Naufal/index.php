67f7834af5447415623c199808e82e550ca0cc84
Muhammad Naufal Hadrian <81348869+naufalhadriann@users.noreply.github.com>
2022-10-25 18:32:46 +0700

    Add files via upload

---
+++ Kelompok/Naufal/index.php
@@ -0,0 +1,15 @@
+<?php
+$a = 80;
+$b = 81;
+if($a % 79) {
+    echo "$a adalah angka genap";
+}else {
+    echo "$a adalah angka ganjil";
+}
+echo "<pre>";
+if($b % 80) {
+    echo "$b adalah angka ganjil";
+}else {
+    echo "$b adalah angka genap";
+}
+?>
